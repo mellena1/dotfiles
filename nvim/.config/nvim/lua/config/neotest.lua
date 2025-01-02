41f612f323832c9a5f1e7dcf0cff2a3005c94de1
Andrew Mellen <andrew_mellen@icloud.com>
2025-01-02 16:29:21 -0500

    add some neotest keybinds

--- nvim/.config/nvim/lua/config/neotest.lua
+++ nvim/.config/nvim/lua/config/neotest.lua
@@ -1,5 +1,14 @@
-require("neotest").setup({
+local neotest = require("neotest")
+
+neotest.setup({
 	adapters = {
-		require("neotest-rust")
+		require("neotest-rust") {
+			args = { "--no-capture" }
+		}
 	}
 })
+
+vim.keymap.set("n", "<leader>rt", neotest.run.run, { desc = "Run the closest test" })
+vim.keymap.set("n", "<leader>ft", function() neotest.run.run(vim.fn.expand("%")) end, { desc = "Run file tests" })
+vim.keymap.set("n", "<leader>to", function() neotest.output.open({ enter = true }) end,
+	{ desc = "Open test output window" })
