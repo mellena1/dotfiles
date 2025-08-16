444ca6de8c405c1595dafd7e4793ad2a94be6fd8
Andrew Mellen <andrew_mellen@icloud.com>
2025-08-16 12:21:56 -0400

    harpoon keybind help

--- nvim/.config/nvim/lua/config/harpoon.lua
+++ nvim/.config/nvim/lua/config/harpoon.lua
@@ -6,14 +6,14 @@ vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
 	{ desc = "Open harpoon window" })
 
-vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end, { desc = "Harpoon 1" })
+vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end, { desc = "Harpoon 2" })
+vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end, { desc = "Harpoon 3" })
+vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end, { desc = "Harpoon 4" })
 
 -- Go to previous and next buffers in the list
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "Prev harpoon" })
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, { desc = "Next harpoon" })
 
 -- basic telescope configuration
 local conf = require("telescope.config").values
